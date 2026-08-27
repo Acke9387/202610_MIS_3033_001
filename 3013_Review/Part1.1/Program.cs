d39815af106950c6e39cc92eb912b4fa9cde1c18
Ackerman, Adam P. <adam@ou.edu>
2026-08-27 09:55:18 -0500

    Completed part 1.1 and 1.2

---
+++ 3013_Review/Part1.1/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Create a console application that calculates the shipping charge for an online order.
+
+    Inputs    
+        Order subtotal
+        Customer type: Standard or Premium
+        Expedited shipping: Yes/No
+        Rules
+    
+    Orders $75+ receive free standard shipping.
+    Otherwise, standard shipping costs $7.99.
+    Premium customers always receive free standard shipping.
+    Expedited shipping adds $12.00 regardless of customer type.
+    Sales tax is 8.5%.
+    
+    Output
+    Display:
+    
+    Subtotal
+    Shipping
+    Tax
+ * 
+ * 
+ */
+
+double subtotal = 0;
+string customerType = "";
+string expeditedShipping = "";
+double shipping = 0;
+double tax = 0;
+
+Console.WriteLine("What is your subtotal? <<");
+string strSubtotal = Console.ReadLine();
+//subtotal = Convert.ToDouble(strSubtotal);
+//subtotal = double.Parse(strSubtotal);
+
+bool isValidNumber = double.TryParse(strSubtotal, out subtotal);
+
+if (isValidNumber  == false)
+{
+    Console.WriteLine("Sorry, your subtotal has to be a valid number.\nPlease try again<<");
+    subtotal = double.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("What is your customer type? (Standard or Premium) <<");
+customerType = Console.ReadLine();
+
+Console.WriteLine("Do you want expedited shipping? (Yes/No) <<");
+expeditedShipping = Console.ReadLine();
+
+if(expeditedShipping.ToLower() == "yes")
+{
+    shipping = 12.00;
+}
+else if( customerType.ToLower() == "premium")
+{
+    shipping = 0;
+}
+else if (subtotal >= 75)
+{
+    shipping = 0;
+}
+else
+{
+    shipping = 7.99;
+}
+
+double total = subtotal + shipping;
+tax = total * 0.085;
+total += tax;
+//total = total + tax;
+
+Console.WriteLine($"Subtotal:\t{subtotal.ToString("C3")}\nShipping:\t{shipping:C3}\nTax:\t\t{tax:C}\nTotal:\t\t{total.ToString("C3")}");
---
+++ 3013_Review/Part1.2/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * A salesperson enters their sales totals for the last five days.
+
+    Store the values in a List<double> and calculate:
+
+    Total sales
+    Average daily sales
+    Highest sales day
+    Lowest sales day
+    Number of days exceeding $1,000
+ */
+
+List<double> salesTotals = new List<double>();
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"Enter sales total for day {i}: ");
+    double sales = Convert.ToDouble(Console.ReadLine());
+    salesTotals.Add(sales);
+}
+
+double total = TotalSales(salesTotals);
+double average = AverageSales(salesTotals);
+double highestSales = HighestSale(salesTotals);
+double lowestSales =  LowestSale(salesTotals);
+int daysHigherThan1000 = CountDaysAboveThreshold(salesTotals);
+int daysHigherThan1500 = CountDaysAboveThreshold(salesTotals, 1500);
+
+Console.WriteLine($"Total sales: {total}");
+Console.WriteLine($"Average daily sales: {average}");
+Console.WriteLine($"Highest sales day: {highestSales}");
+Console.WriteLine($"Lowest sales day: {lowestSales}");
+Console.WriteLine($"Days with sales above $1,000: {daysHigherThan1000}");
+Console.WriteLine($"Days with sales above $1,500: {daysHigherThan1500}");
+
+int CountDaysAboveThreshold(List<double> sales, double threshold = 1000)
+{
+    int count = 0;
+    foreach (double sale in sales)
+    {
+        if (sale > threshold)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+double LowestSale(List<double> r)
+{
+    return r.Min();
+}
+double HighestSale(List<double> q)
+{
+    double highest = q[0];
+    foreach (double sales in q)
+    {
+        if (sales > highest)
+        {
+            highest = sales;
+        }
+    }
+    return highest;
+}
+double AverageSales(List<double> y)
+{
+    double total = TotalSales(y);
+    return total / y.Count;
+}
+double TotalSales(List<double> x)
+{
+    double total = 0;
+    foreach (double sales in x)
+    {
+        total += sales;
+    }
+    return total;
+}
