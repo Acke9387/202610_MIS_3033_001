ea9cee5f50d4050a9c27700a93d7bd257dc1aa52
Ackerman, Adam P. <adam@ou.edu>
2026-09-08 10:07:05 -0500

    did in class example over wpf

---
+++ In_Class_Examples/IntroductionToWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntroductionToWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> all_Foods = new List<string>();
+        string favoriteFood;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] foods = File.ReadAllLines("sales.csv");
+
+            foreach (string food in foods.Skip(1))
+            {
+                lstFoods.Items.Add(food);
+                all_Foods.Add(food);
+            }
+            favoriteFood = "Steak";
+            txtFood.Clear();
+            txtFood.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Button clicked");
+            string foodName = txtFood.Text;
+            if (foodName!= "" || foodName.Length > 2)
+            {
+                lstFoods.Items.Add(foodName); 
+            }
+            txtFood.Text = "";
+            txtFood.Clear();    
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //MessageBox.Show("Text changed", "User Message", MessageBoxButton.CancelTryContinue, MessageBoxImage.Warning);
+            
+        }
+    }
+}
