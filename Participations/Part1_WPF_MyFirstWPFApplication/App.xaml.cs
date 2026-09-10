fbdbcb47ba6d770661d21cf6688ff55628640d4f
Ackerman, Adam P. <adam@ou.edu>
2026-09-10 09:31:40 -0500

    started the participation

---
+++ Participations/Part1_WPF_MyFirstWPFApplication/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Part1_WPF_MyFirstWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Participations/Part1_WPF_MyFirstWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1_WPF_MyFirstWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Read in the data file and populate the listbox
+
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the values from the textboxes and create a new object to add to the listbox
+
+
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            // Get all the products from the listbox and serialize them to a file
+
+            string jsonToWriteToFile = JsonConvert.SerializeObject(lstProducts.Items, Formatting.Indented);
+
+
+        }
+    }
+}
