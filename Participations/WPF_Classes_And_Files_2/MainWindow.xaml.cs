6744a5e39cbdabed680d1d4aa2632838f57dbd97
Ackerman, Adam P. <adam@ou.edu>
2026-09-17 09:57:39 -0500

    Completed participation

---
+++ Participations/WPF_Classes_And_Files_2/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes_And_Files_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Sale> salesList = new List<Sale>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] linesOfFile = File.ReadAllLines("SalesJan2009.csv");
+            for (int i = 1; i < linesOfFile.Length; i++)
+            {
+                //       0            1      2          3      4    5      6     7           8            9         10       11
+                //Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
+                string line = linesOfFile[i];
+                //line = "1/2/09 6:17,Product1,1200,Mastercard,carolina,Basildon,England,United Kingdom,1/2/09 6:00,1/2/09 6:08,51.5,-1.1166667"
+                string[] partsOfLine = linesOfFile[i].Split(',');
+                Sale sale = new Sale();
+                sale.TransactionDate = DateTime.Parse(partsOfLine[0].Trim());
+                sale.Price = double.Parse(partsOfLine[2].Trim());
+                sale.PaymentType = partsOfLine[3].Trim();
+                sale.Name = partsOfLine[4].Trim();
+                sale.Country = partsOfLine[7].Trim();
+                salesList.Add(sale);
+            }
+
+            PopulateListBox("Amex");
+            PopulateListBox("Visa");
+            PopulateListBox("Mastercard");
+            PopulateListBox("Diners");
+
+        }
+
+        private void PopulateListBox(string paymentType)
+        {
+            foreach (Sale s in salesList)
+            {
+
+                if (paymentType.ToLower() == "amex")
+                {
+                    if (s.PaymentType.ToLower() == "amex")
+                    {
+                        lstAmex.Items.Add(s);
+                    }
+                }
+                else if (paymentType.ToLower() == "visa")
+                {
+                    if (s.PaymentType.ToLower() == "visa")
+                    {
+                        lstVisa.Items.Add(s);
+                    }
+                }
+                else if (paymentType.ToLower() == "diners")
+                {
+                    if (s.PaymentType.ToLower() == "diners")
+                    {
+                        lstDiners.Items.Add(s);
+                    }
+                }
+                else if (paymentType.ToLower() == "mastercard")
+                {
+                    if (s.PaymentType.ToLower() == "mastercard")
+                    {
+                        lstMastercard.Items.Add(s);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Participations/WPF_Classes_And_Files_2/Sale.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes_And_Files_2
+{
+    public class Sale
+    {
+
+        public DateTime TransactionDate { get; set; }
+        public double Price { get; set; }
+        public string PaymentType { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        //default constructor / empty constructor
+        public Sale()
+        {
+            TransactionDate = DateTime.Now;
+            Price = 0.0;
+            PaymentType = string.Empty;
+            Name = string.Empty;
+            Country = string.Empty;
+        }
+
+        public Sale(double price, string pType) : this()
+        {
+            TransactionDate = DateTime.Now;
+            Name = string.Empty;
+            Country = string.Empty;
+            Price = price;
+            PaymentType = pType;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{PaymentType[0]} {Name} - {Price.ToString("C")}";
+        }
+
+    }
+}
